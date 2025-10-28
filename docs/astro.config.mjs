435e01b3cb3c4df016ac1d30f5dc8d94734153a7
gregoryw3 <greg.wainer@gmail.com>
2025-10-28 18:23:12 -0400

    Updated documentation site url again

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://rocketryvt.github.io/RocketryVT/Avionics',
+  site: 'https://rocketryatvirginiatech.org/Avionics',
   base: '/Avionics',
   integrations: [
       starlight({
